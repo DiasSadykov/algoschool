ad5bfc01636d73d46cbb6fcf37adb4de967d9a67
Dias Sadykov <dias.sadykov0@gmail.com>
2022-02-14 23:55:29 +0300

    course centering fixed

--- algoschool-frontend/src/Components/CourseBlock/CourseBlock.tsx
+++ algoschool-frontend/src/Components/CourseBlock/CourseBlock.tsx
@@ -14,7 +14,7 @@ type Props = {
 const renderProblems = (blockItems: BlockItem[], blockTitle: string, completedProblems: Set<string>) => {
     return (
         blockItems.map((blockItem) => 
-                <Link key={blockItem.itemId} className="mm-bullet dark:text-gray-200 text-gray-700 pt-2 pb-2 rounded-md pr-8 mr-2 pl-12 dark:hover:text-gray-900 hover:text-gray-900 md:text-lg hover:bg-blue-100 transition easy-in-out duration-100"  to={`/${blockItem.itemType}/${blockItem.itemId}`}>{blockItem.itemTitle}</Link>
+                <Link key={blockItem.itemId} className="absolute right-6 mm-bullet dark:text-gray-200 text-gray-700 pt-2 pb-2 rounded-md pr-8 mr-2 pl-12 dark:hover:text-gray-900 hover:text-gray-900 md:text-lg hover:bg-blue-100 transition easy-in-out duration-100"  to={`/${blockItem.itemType}/${blockItem.itemId}`}>{blockItem.itemTitle}</Link>
             )
     )
 }
@@ -23,9 +23,9 @@ function CourseBlock(props: Props) {
     const completedProblems = useSelector(getCompletedProblems)
 
     return (
-        <div className="flex flex-row justify-center pl-4" >
-            <p className="dark:text-gray-50 pr-4 pt-2 font-semibold text-xl">{props.sectionTitle}</p>
-            <div className="flex flex-col">
+        <div className="flex flex-row w-screen" >
+            <p className="dark:text-gray-50 pr-10 pt-2 font-semibold text-xl w-1/2 text-right">{props.sectionTitle}</p>
+            <div className="w-1/2 sm:w-auto flex flex-col">
                     {renderProblems(props.blockItems, props.sectionTitle, completedProblems)}
             </div>
 
